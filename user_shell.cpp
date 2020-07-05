0c9efbd6404a1bed818950ef6ff29f821fcb2d64
Yanchang Li <yanchang.li@zongmutech.com>
2020-07-05 16:37:57 +0800

    add symbol link file for utils
    Signed-off-by: Yanchang Li <yanchang.li@zongmutech.com>

--- user_shell.cpp
+++ user_shell.cpp
@@ -4,7 +4,9 @@
 int user_cmd_handler(int argc, char** argv)
 {
     DPR(DD, "user argS [%d]: %s %s  %s\n", argc, argv[0], argv[1], argv[2]);
-    if(strstr(argv[0], "ispreg")) {
+
+    if (strstr(argv[0], "ispreg")) {
+        
         unsigned char val;
         int reg;
     
@@ -16,7 +18,23 @@ int user_cmd_handler(int argc, char** argv)
             cam_write(atoi(argv[1]), TARGET_ISP, strtol(argv[2], NULL, 16), strtol(argv[3], NULL, 16));
             printf("%02x", val);
         }
+
+    } else if (strstr(argv[0], "flashreg")) {
+        
+        unsigned char val;
+        int reg;
+        
+        if (argc == 3) {
+            cam_read(atoi(argv[1]), TARGET_FLASH, strtol(argv[2], NULL, 16), &val);
+            printf("%02x", val);
+
+        } else if (argc == 4) {
+            cam_write(atoi(argv[1]), TARGET_FLASH, strtol(argv[2], NULL, 16), strtol(argv[3], NULL, 16));
+            printf("%02x", val);
+        }
+
     } else {
+
         PR("Error! cmds not recognized!\n");
     }
 
